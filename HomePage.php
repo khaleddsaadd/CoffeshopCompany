ca24fd2fd3b811e9651d66f4898103585c1d547e
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-06-02 23:18:08 +0200

    homepage.php&.css

---
+++ HomePage.php
@@ -0,0 +1,40 @@
+<?php require_once("HomePageClass.php");
+ ?>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="HomePageCss.css">
+    </head>
+    <body>
+        <div id="product-grid">
+            <?php 
+                $allproducts =HomeProducts::AllProducts();
+                foreach($allproducts as $product)
+                {
+            ?>
+                   <div class="product-item" width="200px">
+                        <form method="Post" action="HomePage.php?id=<?php echo $product->id;?>&pr=<?php echo $product->price;?>">
+                            <img class ="P_IMG"src="images/<?php echo $product -> image?>"><br><br>
+                            <center>
+                            <label class="Name"><?php echo $product -> name ?></label> <br><br>
+                            <label class="Price"><?php echo $product -> price ?> EGP</label> <br><br>
+                            <input type="number" name="quantity" placeholder="Quantity" value="1"><br><br>
+                            <input type="submit" value ="Add to cart" name="ID"> <br>
+                            </center>
+                        </form>
+                   </div> 
+            <?php 
+                }
+                if($_POST)
+                {
+                    $id= $_GET["id"];
+                    $pr = $_GET["pr"];
+                    $quantity = $_POST['quantity'];
+                    $items = new HomeProducts($id);
+                    $items -> addtocart($id,$pr,$quantity);
+
+                }
+            ?>
+        </div>
+    </body>
+</html>
