e4c6eddc7eab665fe4e509a3956537c0ba4380c9
Philip-G-Hanna <philip1805061@miuegypt.edu.eg>
2021-06-02 22:47:14 +0200

    little updates

--- coffee_shop.sql
+++ coffee_shop.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 04:33 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Jun 02, 2021 at 10:33 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,21 +33,17 @@ CREATE TABLE `cart` (
   `User_id` int(11) NOT NULL,
   `P_Id` int(11) NOT NULL,
   `Total_Price` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `Quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `orders`
+-- Dumping data for table `cart`
 --
 
-CREATE TABLE `orders` (
-  `O_id` int(11) NOT NULL,
-  `User_id` int(11) NOT NULL,
-  `P_Id` int(11) NOT NULL,
-  `Total_Price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `cart` (`ID`, `User_id`, `P_Id`, `Total_Price`, `Quantity`) VALUES
+(17, 0, 55, 120, 9),
+(18, 0, 55, 120, 9),
+(19, 0, 3, 1, 45);
 
 -- --------------------------------------------------------
 
@@ -58,8 +55,21 @@ CREATE TABLE `products` (
   `P_Id` int(11) NOT NULL,
   `P_Name` varchar(255) NOT NULL,
   `P_Price` int(11) NOT NULL,
-  `P_Image` varchar(255) NOT NULL
+  `P_Image` varchar(255) NOT NULL,
+  `P_Description` varchar(255) NOT NULL,
+  `P_Ingredients` varchar(255) NOT NULL,
+  `P_Beverages` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`P_Id`, `P_Name`, `P_Price`, `P_Image`, `P_Description`, `P_Ingredients`, `P_Beverages`) VALUES
+(1, 'Latte', 35, 'cup.jpg', 'qqqqqqqqqmaajs.abhk s, asn,dnsa', 'qss, an f', 'jsankdsna'),
+(3, 'Cappuccino', 45, 'cappu.jpg', '', '', ''),
+(4, 'Mocha', 50, 'mocha.jpg', '', '', ''),
+(5, 'yyy', 120, '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -82,17 +92,7 @@ CREATE TABLE `users` (
 -- Indexes for table `cart`
 --
 ALTER TABLE `cart`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `User_id` (`User_id`),
-  ADD KEY `P_Id` (`P_Id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`O_id`),
-  ADD KEY `User_id` (`User_id`),
-  ADD KEY `P_Id` (`P_Id`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `products`
@@ -114,43 +114,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `O_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `P_Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `P_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `User_Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_Id`),
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`P_Id`) REFERENCES `products` (`P_Id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`User_id`) REFERENCES `users` (`User_Id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`P_Id`) REFERENCES `cart` (`P_Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- dbclass.php
+++ dbclass.php
@@ -5,7 +5,6 @@ class Database
 	var $user = "root";
 	var $pass = "";
 	var $db = "coffee_shop";
-	var $tbname = "products";
 	public function connect()
 	{
 		$con = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
@@ -14,7 +13,7 @@ class Database
 	public function saveRecords($N,$P,$I,$D,$In,$B)
 	{
 		$conn = $this->connect();
-		mysqli_query($conn, "insert into ".$this->tbname."(P_Name,P_Price,P_Image,P_Description,P_Ingredients,P_Beverages) values('$N','$P','$I','$D','$In','$B')") or die(mysqli_error($conn));
+		mysqli_query($conn, "insert into products(P_Name,P_Price,P_Image,P_Description,P_Ingredients,P_Beverages) values('$N','$P','$I','$D','$In','$B')") or die(mysqli_error($conn));
 		echo "<div style = 'padding:20px; background-color:yellow;'>
 		Data Added Successfully</div>";
 	}
